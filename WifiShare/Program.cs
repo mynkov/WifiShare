6d13256b676ae44b10d49245f8e70d9d56f52756
mynkov <mynkovas@mail.ru>
2017-10-29 13:42:06 +0300

    Create project

---
+++ WifiShare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WifiShare
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var psi = new ProcessStartInfo
+                {
+                    FileName = "netsh",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    Arguments = "wlan start hostednetwork"
+                };
+
+            var proc = Process.Start(psi);
+            proc.WaitForExit();
+            string errorOutput = proc.StandardError.ReadToEnd();
+            string standardOutput = proc.StandardOutput.ReadToEnd();
+            if (proc.ExitCode != 0)
+                throw new Exception("netsh exit code: " + proc.ExitCode.ToString() + " " +
+                                    (!string.IsNullOrEmpty(errorOutput) ? " " + errorOutput : "") + " " +
+                                    (!string.IsNullOrEmpty(standardOutput) ? " " + standardOutput : ""));
+        }
+    }
+}
